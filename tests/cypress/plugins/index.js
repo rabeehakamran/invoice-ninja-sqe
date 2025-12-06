81b8ca37da6797a5bbea496269aba7a9cb3cd7b4
Rabeeha Kamran <rabeehakamran@gmail801.com>
2025-12-06 21:38:38 +0500

    updated index.js

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -7,6 +7,19 @@ module.exports = (on, config) => {
     // Removed swap-env as it does not exist in your project
     // and is not needed for your custom SQE tests.
 
+    // --- CYPRESS TASK REGISTRATION ---
+    // The following tasks are needed to prevent the 'cy.task not registered' error
+    // from your support/index.js file (activateCypressEnvFile, activateLocalEnvFile).
+    on("task", {
+        "activateCypressEnvFile": () => { 
+            console.log("CYPRESS TASK: activateCypressEnvFile called (Mocked)"); 
+            return null; 
+        },
+        "activateLocalEnvFile": () => { 
+            console.log("CYPRESS TASK: activateLocalEnvFile called (Mocked)"); 
+            return null; 
+        }
+    });
+
     return config;
 };
-
